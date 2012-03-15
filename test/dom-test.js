2d6697ff9611d6922d71fe720cc8ed55ac079c9d
Christian Johansen <=>
2012-03-15 13:54:11 +0100

    Add dom.cn.rm, and make class name method names nice and short

--- test/dom-test.js
+++ test/dom-test.js
@@ -3,48 +3,88 @@ if (typeof require === "function" && typeof module !== "undefined") {
     var cull = require("../lib/cull");
 }
 
-buster.testCase("DOM", {
-    "add class": {
-        "sets initial class name": function () {
-            var div = { className: "" };
-            cull.dom.addClassName("something", div);
-            assert.className(div, "something");
+(function (cn) {
+    buster.testCase("DOM", {
+        "add class": {
+            "sets initial class name": function () {
+                var div = { className: "" };
+                cn.add("something", div);
+                assert.className(div, "something");
+            },
+
+            "adds additional class name": function () {
+                var div = { className: "hey" };
+                cn.add("something", div);
+                assert.className(div, "hey something");
+            },
+
+            "does not add duplicate class name": function () {
+                var div = { className: "hey" };
+                cn.add("hey", div);
+                assert.equals(div.className, "hey");
+            },
+
+            "adds class name to all elements": function () {
+                var divs = [
+                    { className: "hey" },
+                    { className: "" },
+                    { className: "meh heh" }
+                ];
+                cn.add("hola", divs);
+                assert.equals(divs[0].className, "hey hola");
+                assert.equals(divs[1].className, "hola");
+                assert.equals(divs[2].className, "meh heh hola");
+            },
+
+            "does not duplicate class name for element in seq": function () {
+                var divs = [
+                    { className: "hey" },
+                    { className: "" },
+                    { className: "meh heh" }
+                ];
+                cn.add("hey", divs);
+                assert.equals(divs[0].className, "hey");
+                assert.equals(divs[1].className, "hey");
+                assert.equals(divs[2].className, "meh heh hey");
+            }
         },
 
-        "adds additional class name": function () {
-            var div = { className: "hey" };
-            cull.dom.addClassName("something", div);
-            assert.className(div, "hey something");
-        },
+        "remove class": {
+            "does nothing when element has no classes": function () {
+                var div = { className: "" };
+                cn.rm("something", div);
+                assert.equals(div.className, "");
+            },
 
-        "does not add duplicate class name": function () {
-            var div = { className: "hey" };
-            cull.dom.addClassName("hey", div);
-            assert.equals(div.className, "hey");
-        },
+            "removes only matching class": function () {
+                var div = { className: "something" };
+                cn.rm("something", div);
+                assert.equals(div.className, "");
+            },
 
-        "adds class name to all elements": function () {
-            var divs = [
-                { className: "hey" },
-                { className: "" },
-                { className: "meh heh" }
-            ];
-            cull.dom.addClassName("hola", divs);
-            assert.equals(divs[0].className, "hey hola");
-            assert.equals(divs[1].className, "hola");
-            assert.equals(divs[2].className, "meh heh hola");
-        },
+            "removes matching class": function () {
+                var div = { className: "hey something there" };
+                cn.rm("something", div);
+                assert.equals(div.className, "hey there");
+            },
 
-        "does not duplicate class name for element in seq": function () {
-            var divs = [
-                { className: "hey" },
-                { className: "" },
-                { className: "meh heh" }
-            ];
-            cull.dom.addClassName("hey", divs);
-            assert.equals(divs[0].className, "hey");
-            assert.equals(divs[1].className, "hey");
-            assert.equals(divs[2].className, "meh heh hey");
+            "removes matching class when last": function () {
+                var div = { className: "something there" };
+                cn.rm("there", div);
+                assert.equals(div.className, "something");
+            },
+
+            "removes matching class from all elements": function () {
+                var divs = [
+                    { className: "meh" },
+                    { className: "some thing eh" },
+                    { className: "heh meh" }
+                ];
+                cn.rm("meh", divs);
+                assert.equals(divs[0].className, "");
+                assert.equals(divs[1].className, "some thing eh");
+                assert.equals(divs[2].className, "heh");
+            }
         }
-    }
-});
+    });
+}(cull.dom.cn));
