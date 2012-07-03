237d7a7131726ce41584424214e9f4efbea4be9f
Magnar Sveen <magnars@gmail.com>
2012-07-03 15:05:01 +0200

    Clean up tests by using predefined tag functions.

--- test/browser/el-test.js
+++ test/browser/el-test.js
@@ -1,6 +1,8 @@
 /*global document*/
 
 (function (d) {
+    var div = d.el.div;
+
     buster.testCase("Element", {
         "setProp": {
             "sets property on element": function () {
@@ -60,54 +62,45 @@
 
         "builder": {
             "creates div element": function () {
-                var div = cull.partial(d.el, "div");
                 assert.tagName(div(), "div");
             },
 
             "creates element with attribute property": function () {
-                var a = cull.partial(d.el, "a");
-                assert.match(a({ href: "/yo" }).href, /\/yo$/);
+                assert.match(d.el.a({ href: "/yo" }).href, /\/yo$/);
             },
 
             "creates element with multiple attribute properties": function () {
-                var a = cull.partial(d.el, "a");
-                var link = a({ href: "/yo", className: "link-thingie" });
+                var link = d.el.a({ href: "/yo", className: "link-thingie" });
 
                 assert.className(link, "link-thingie");
                 assert.match(link.href, /\/yo$/);
             },
 
             "creates element with child": function () {
-                var div = cull.partial(d.el, "div");
                 var el = div(div());
 
                 assert.equals(el.childNodes.length, 1);
             },
 
             "creates element with children": function () {
-                var div = cull.partial(d.el, "div");
                 var el = div([div(), div()]);
 
                 assert.equals(el.childNodes.length, 2);
             },
 
             "complains about multiple element arguments": function () {
-                var div = cull.partial(d.el, "div");
                 assert.exception(function () {
                     div(div(), div(), div(), div());
                 }, "TypeError");
             },
 
             "complains about tagName property for element": function () {
-                var div = cull.partial(d.el, "div");
-
                 assert.exception(function () {
                     div(div(), div());
                 }, "TypeError");
             },
 
             "creates element with children and properties": function () {
-                var div = cull.partial(d.el, "div");
                 var el = div({ className: "hey", id: "ho" }, div());
 
                 assert.equals(el.childNodes.length, 1);
@@ -116,35 +109,30 @@
             },
 
             "creates element with text content": function () {
-                var h2 = cull.partial(d.el, "h2");
-                var el = h2("Hey man");
+                var el = d.el.h2("Hey man");
 
                 assert.equals(el.innerHTML, "Hey man");
             },
 
             "should not insert text as html": function () {
-                var div = cull.partial(d.el, "div");
                 var el = div("<p>x</p><p>s</p><p>s</p>");
 
                 assert.equals(el.childNodes.length, 1);
             },
 
             "creates element with text and DOM element content": function () {
-                var div = cull.partial(d.el, "div");
                 var el = div(["Hey man", div(), "Awright"]);
 
                 assert.match(el.innerHTML, /hey man\s*<div><\/div>awright/i);
             },
 
             "sets style properties": function () {
-                var div = cull.partial(d.el, "div");
                 var el = div({ style: { position: "relative" } });
 
                 assert.equals(el.style.position, "relative");
             },
 
             "creates element with customly handled attribute": function () {
-                var div = cull.partial(d.el, "div");
                 d.el.propmap["for"] = function (el, attr) {
                     el.htmlFor = attr;
                 };
@@ -155,9 +143,10 @@
             },
 
             "creates div element directly": function () {
-                var div = d.el("div", { className: "Yay" });
-                assert.tagName(div, "div");
-                assert.className(div, "Yay");
+                var el = d.el("div", { className: "Yay" });
+
+                assert.tagName(el, "div");
+                assert.className(el, "Yay");
             }
         }
     });
